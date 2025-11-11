6fae808b0fb0612c877d350b9091d0bd2793db29
Redoanur <redoanur03@gmail.com>
2025-11-11 21:23:44 +0600

    Connect the database with server

--- index.js
+++ index.js
@@ -1,12 +1,39 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 1212;
+// console.log(process.env);
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.8l6zy8s.mongodb.net/?appName=Cluster0`;
+
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
 
 app.get('/', (req, res) => {
     res.send('KrishiLink server is running')
-})
+});
+
+async function run() {
+    try {
+        await client.connect();
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    }
+    finally {
+    }
+}
+run().catch(console.dir);
 
 app.listen(port, () => {
     console.log(`KrishiLink server is running on port: ${port}`)
-})
+});
+
+//iIlKZzSOS0OF856E
+//krishilinkdb
